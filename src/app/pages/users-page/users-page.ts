0d694f874e1346789f162d5f6cb5ec477941db1d
Ghitha Dinan <ghithadinanhs@gmail.com>
2026-04-09 16:55:55 +0700

    page fixing

--- src/app/pages/users-page/users-page.ts
+++ src/app/pages/users-page/users-page.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
-import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
-import { MatButton } from '@angular/material/button';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
 
 @Component({
   selector: 'app-users-page',
-  imports: [MatFormField, MatLabel, MatInput, MatButton],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatPaginatorModule,
+    MatSortModule,
+  ],
   templateUrl: './users-page.html',
   styleUrl: './users-page.css',
 })
-export class UsersPage {}
+export class UsersPage implements AfterViewInit {
+  displayedColumns: string[] = ['id', 'name', 'email'];
+
+  dataSource = new MatTableDataSource([
+    { id: 1, name: 'Ghitha', email: 'ghitha@mail.com' },
+    { id: 2, name: 'User 2', email: 'user2@mail.com' },
+    { id: 3, name: 'User 3', email: 'user3@mail.com' },
+    { id: 4, name: 'User 4', email: 'user4@mail.com' },
+    { id: 5, name: 'User 5', email: 'user5@mail.com' },
+  ]);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = value.trim().toLowerCase();
+  }
+}
